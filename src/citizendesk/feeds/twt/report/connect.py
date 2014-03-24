db6dff9eede1e3d1bb394e0d031a5dc81c3920d9
Martin Saturka <martin.saturka@sourcefabric.org>
2014-03-24 14:58:46 +0100

    Adjusted report access

--- src/citizendesk/feeds/twt/report/connect.py
+++ src/citizendesk/feeds/twt/report/connect.py
@@ -50,8 +50,6 @@ def feed_twt_report_get_one(report_id):
         return (json.dumps(ret_data, default=json_util.default, sort_keys=True), 404, {'Content-Type': 'application/json'})
 
     ret_data = {'_meta': {'schema': process.schema}, '_data': res[1]}
-    if 3 >= len(res):
-        ret_data['_meta']['list'] = res[2]
     return (json.dumps(ret_data, default=json_util.default, sort_keys=True), 200, {'Content-Type': 'application/json'})
 
 @bp_feed_twt_report.route('/feeds/twt/endpoint/<endpoint_id>', defaults={'is_proto': None}, methods=['GET'], strict_slashes=False)
@@ -107,6 +105,8 @@ def feed_twt_report_get_session(session_id):
         return (json.dumps(ret_data, default=json_util.default, sort_keys=True), 404, {'Content-Type': 'application/json'})
 
     ret_data = {'_meta': {'schema': process.schema}, '_data': res[1]}
+    if 3 >= len(res):
+        ret_data['_meta']['list'] = res[2]
     return (json.dumps(ret_data, default=json_util.default, sort_keys=True), 200, {'Content-Type': 'application/json'})
 
 @bp_feed_twt_report.route('/feeds/twt/report/<report_id>', defaults={}, methods=['PATCH'], strict_slashes=False)
